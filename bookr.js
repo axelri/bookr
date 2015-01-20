e19f70bded0fb2ab937e7060b6dba82957e1e5d9
axelri <axel.riese@gmail.com>
2015-01-21 00:13:56 +0100

    multiple jobs

--- bookr.js
+++ bookr.js
@@ -1,6 +1,6 @@
-var casper = require("casper").create();
 var utils = require("utils");
 var _ = require("lodash");
+var bookings = require("./bookings.json");
 
 var url = 'http://bokning.lib.kth.se/bokning_po.asp?bibid=KTHB&typ=Grp';
 var elems = {
@@ -11,69 +11,107 @@ var elems = {
   bookForm: 'form[action="bokaupd_po.asp"]'
 };
 
-var day = "on";
-var time = "12";
-var room = "12";
-var card = "";
-var name = "bookr";
+var finished = 0;
 
-casper.start(url);
+_.forEach(bookings, function(b) {
 
-// select day and submit
-casper.then(function() {
-  var els = this.getElementsInfo(elems.selectId + " > option");
-  var dayOp = _.find(els, function(e) {
-    var reg = new RegExp(day+"$");
-    return e.text.match(reg) !== null;
+  var casper = require('casper').create();
+  var fail = false;
+  var failReason = "";
+  casper.start(url);
+  casper.then(function() {
+    if (fail) {return;}
+
+    var els = this.getElementsInfo(elems.selectId + " > option");
+    var dayOp = _.find(els, function(e) {
+      var reg = new RegExp(b.day+"$");
+      return e.text.match(reg) !== null;
+    });
+    if (dayOp === undefined) {
+      failReason = "Can't find requested date";
+      fail = true;
+      return;
+    }
+
+    var fills = {};
+    fills[elems.selectName] = dayOp.attributes.value;
+    this.fill(elems.dateForm, fills, true);
   });
-  if (dayOp === undefined) {
-    this.die("Can't find requested date");
-  }
 
-  var fills = {};
-  fills[elems.selectName] = dayOp.attributes.value;
-  this.fill(elems.dateForm, fills, true);
-});
+  casper.then(function() {
+    if (fail) {return;}
 
-// follow link to room and time
-casper.then(function() {
-  var cols = this.getElementsInfo(elems.table +
-      " tr:first-child > td > span");
-  var colIndex = _.findIndex(cols, function(e) {
-    var reg = new RegExp(time+":00 -");
-    return e.text.match(reg) !== null;
+    // follow link to room and time
+    var cols = this.getElementsInfo(elems.table +
+        " tr:first-child > td > span");
+    var colIndex = _.findIndex(cols, function(e) {
+      var reg = new RegExp(b.time+":00 -");
+      return e.text.match(reg) !== null;
+    });
+    if (colIndex === -1) {
+      failReason = "Can't find requested time";
+      fail = true;
+      return;
+    }
+    var colChild = colIndex + 2;
+
+    var rows = this.getElementsInfo(elems.table +
+      " > tr td:first-child b:first-child");
+    var rowIndex = _.findIndex(rows, function(e) {
+      var reg = new RegExp("^"+b.room);
+      return e.text.match(reg) !== null;
+    });
+    if (rowIndex === -1) {
+      failReason = ("Can't find requested room");
+      fail = true;
+      return;
+    }
+    var rowChild = rowIndex + 2;
+
+    var button = elems.table + " tr:nth-of-type(" + rowChild + ")" +
+        " td:nth-of-type(" + colChild + ") a";
+    var available = this.exists(button);
+    if (!available) {
+      failReason = "The room is already booked";
+      fail = true;
+      return;
+    }
+    this.click(button);
   });
-  if (colIndex === -1) {
-    this.die("Can't find requested time");
-  }
-  var colChild = colIndex + 2;
 
-  var rows = this.getElementsInfo(elems.table +
-    " > tr td:first-child b:first-child");
-  var rowIndex = _.findIndex(rows, function(e) {
-    var reg = new RegExp("^"+room);
-    return e.text.match(reg) !== null;
+  casper.then(function() {
+    if (fail) {return;}
+
+    this.fill(elems.bookForm, {
+      loan: b.card,
+      anv: b.name
+    }, true);
   });
-  if (rowIndex === -1) {
-    this.die("Can't find requested room");
-  }
-  var rowChild = rowIndex + 2;
 
-  var button = elems.table + " tr:nth-of-type(" + rowChild + ")" +
-      " td:nth-of-type(" + colChild + ") a";
-  var available = this.exists(button);
-  if (!available) {
-    this.die("The room is already booked");
-  }
-  this.click(button);
+  casper.then(function() {
+    if (fail) {return;}
+
+    var error = "p > span";
+    if (this.exists(error)) {
+      var el = this.getElementInfo(error);
+      if (el.text.match(new RegExp("Felaktigt"))) {
+        failReason = "Invalid card number";
+        fail = true;
+        return;
+      }
+    }
+  });
+
+  casper.run(function() {
+    utils.dump(b);
+    if (!fail) {
+      this.echo("Successful booking");
+    } else {
+      this.echo("Booking error: " + failReason);
+    }
+    finished += 1;
+    if (finished >= bookings.length) {
+      this.exit();
+    }
+  });
 });
-
-casper.then(function() {
-  this.fill(elems.bookForm, {
-    loan: card,
-    anv: name
-  }, true);
-  this.echo("Successful booking");
-});
-
-casper.run();

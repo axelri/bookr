8344b320acf303f9fbeff4bc6eace7c6b557e01d
axelri <axel.riese@gmail.com>
2015-01-20 22:35:22 +0100

    almost done

--- bookr.js
+++ bookr.js
@@ -6,20 +6,22 @@ var url = 'http://bokning.lib.kth.se/bokning_po.asp?bibid=KTHB&typ=Grp';
 var elems = {
   dateForm: "form#showbookings",
   selectName: "bokdag",
-  selectId: "#seldate"
+  selectId: "#seldate",
+  table: "center + table > tbody"
 };
+
 var day = "on";
-var time = "13";
+var time = "12";
 var room = "12";
 
-casper.start(url, function() {
-  this.echo(this.getTitle());
-});
+casper.start(url);
 
+// select day and submit
 casper.then(function() {
   var els = this.getElementsInfo(elems.selectId + " > option");
   var dayOp = _.find(els, function(e) {
-    return e.text.match(new RegExp(day+"$")) !== null;
+    var reg = new RegExp(day+"$");
+    return e.text.match(reg) !== null;
   });
   if (dayOp === undefined) {
     this.die("Can't find requested date");
@@ -28,6 +30,40 @@ casper.then(function() {
   var fills = {};
   fills[elems.selectName] = dayOp.attributes.value;
   this.fill(elems.dateForm, fills, true);
+});
+
+// follow link to room and time
+casper.then(function() {
+  var cols = this.getElementsInfo(elems.table +
+      " tr:first-child > td > span");
+  var colIndex = _.findIndex(cols, function(e) {
+    var reg = new RegExp(time+":00 -");
+    return e.text.match(reg) !== null;
+  });
+  if (colIndex === -1) {
+    this.die("Can't find requested time");
+  }
+  var colChild = colIndex + 2;
+
+  var rows = this.getElementsInfo(elems.table +
+    " > tr td:first-child b:first-child");
+  var rowIndex = _.findIndex(rows, function(e) {
+    var reg = new RegExp("^"+room);
+    return e.text.match(reg) !== null;
+  });
+  if (rowIndex === -1) {
+    this.die("Can't find requested room");
+  }
+  var rowChild = rowIndex + 2;
+
+  var button = elems.table + " tr:nth-of-type(" + rowChild + ")" +
+      " td:nth-of-type(" + colChild + ") a";
+  utils.dump(button);
+  var available = this.exists(button);
+  if (!available) {
+    this.die("The room is already booked");
+  }
+  this.click(button);
 });
 
 casper.then(function() {
